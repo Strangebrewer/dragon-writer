1131e1c6de60b87e20bea485e6264dc749ec6d69
Strangebrewer <BKAShambala@gmail.com>
2018-11-27 01:08:44 -0700

    tinkering with routes and studying the render prop pattern.

--- client/src/components/Elements/Page.jsx
+++ client/src/components/Elements/Page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navbar } from './Navbar';
 import SubjectList from "../SubjectList";
 import styled from "styled-components";
-import { GlobalStyle } from "../../GlobalStyle";
+import { GlobalStyle } from "../../utils";
 
 const PageContainer = styled.div`
   min-height: 100vh;
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,6 @@
 export * from "./AddPropsToRoute";
 export * from "./API";
 export * from "./DragonScales";
+export * from "./GlobalStyle";
 export * from "./Themes";
 export * from "./Utils";

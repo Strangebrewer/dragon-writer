7534373dd5582aab119d6c33e94a78319b2b7672
Strangebrewer <BKAShambala@gmail.com>
2018-12-06 13:20:54 -0700

    all image uploading and deleting in place and functioning properly (thus far)

---
+++ client/build/precache-manifest.9a023f60ef8767a6b300534339e919b5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0070f4c8bf4a8b6d5f1e",
+    "url": "/static/js/main.0070f4c8.chunk.js"
+  },
+  {
+    "revision": "346511f07ac707c65661",
+    "url": "/static/js/1.346511f0.chunk.js"
+  },
+  {
+    "revision": "e3c816620278593561c6d1acd67dd86a",
+    "url": "/index.html"
+  }
+];

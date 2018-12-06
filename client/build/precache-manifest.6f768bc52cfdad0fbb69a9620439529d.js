3db4d13bcbaf84a0a2ae01bef917380a2d064d69
Strangebrewer <BKAShambala@gmail.com>
2018-12-06 13:50:31 -0700

    deploying to heroku

---
+++ client/build/precache-manifest.6f768bc52cfdad0fbb69a9620439529d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "98a63e538aa5d8b620c7",
+    "url": "/static/js/main.98a63e53.chunk.js"
+  },
+  {
+    "revision": "346511f07ac707c65661",
+    "url": "/static/js/1.346511f0.chunk.js"
+  },
+  {
+    "revision": "cb5af15f9e1357642ebe9751740e87bf",
+    "url": "/index.html"
+  }
+];
